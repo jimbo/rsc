48666effb2a2c328e16efd699014452f312231a8
Jimmy Sanford <jimbo@users.noreply.github.com>
2022-01-06 16:31:42 -0700

    Clean up category blocks

--- data/home.js
+++ data/home.js
@@ -21,24 +21,39 @@ export default `
 	</div>
 	<div class="cms-row">
 		<div class="cms-cell">
-			<div class="cms-block">
-				<h2 class="cms-block-title">Headline goes here</h2>
-				<p class="cms-block-subtitle">Optional metadata for subtitle should be kept to one or two lines</p>
-				<a class="cms-block-action" href="/pdp">ACTION</a>
+			<div class="cms-callout">
+				<div class="cms-callout-image">
+				</div>
+				<div class="cms-callout-content">
+					<div class="cms-callout-content-block">
+						<h3 class="cms-callout-title">Category title</h2>
+						<p class="cms-callout-subtitle">Text should be kept to 1 line</p>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="cms-cell">
-			<div class="cms-block">
-				<h2 class="cms-block-title">Headline goes here</h2>
-				<p class="cms-block-subtitle">Optional metadata for subtitle should be kept to one or two lines</p>
-				<a class="cms-block-action" href="/pdp">ACTION</a>
+			<div class="cms-callout">
+				<div class="cms-callout-image">
+				</div>
+				<div class="cms-callout-content">
+					<div class="cms-callout-content-block">
+						<h3 class="cms-callout-title">Category title</h2>
+						<p class="cms-callout-subtitle">Text should be kept to 1 line</p>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="cms-cell">
-			<div class="cms-block">
-				<h2 class="cms-block-title">Headline goes here</h2>
-				<p class="cms-block-subtitle">Optional metadata for subtitle should be kept to one or two lines</p>
-				<a class="cms-block-action" href="/pdp">ACTION</a>
+			<div class="cms-callout">
+				<div class="cms-callout-image">
+				</div>
+				<div class="cms-callout-content">
+					<div class="cms-callout-content-block">
+						<h3 class="cms-callout-title">Category title</h2>
+						<p class="cms-callout-subtitle">Text should be kept to 1 line</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
