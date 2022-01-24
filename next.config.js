da90471fa9d59051c4622a927ea5de2162a7b17c
Jimmy Sanford <jimbo@users.noreply.github.com>
2022-01-24 14:10:58 -0700

    Enable custom server

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ module.exports = {
 	env: {
 		gql: "https://commerce-beta.adobe.io/api/MicroconfTenant/graphql?api_key=commerce-graphql-onboarding"
 	},
-	experimental: {
-		concurrentFeatures: true,
-		reactRoot: true,
-		serverComponents: true
-	},
+	// experimental: {
+	// 	concurrentFeatures: true,
+	// 	reactRoot: true,
+	// 	serverComponents: true
+	// },
 	images: {
 		deviceSizes: [320, 480, 640, 800, 960, 1120, 1280, 1440, 1600, 1920],
 		domains: ["jnz3dtiuj77ca.dummycachetest.com"]
